a3b4ae9d19ba3a16c26ddd49df169fe6808c9fb2
azul012 <lavadoa884@gmail.com>
2024-10-30 12:35:36 -0500

    Validacion tienda
    Validacion de los campos en los atributos de tienda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { OfertaComponent } from './components/oferta/oferta.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
+import { CreaeditaTiendaComponent } from './components/tienda/creaedita-tienda/creaedita-tienda.component';
 
 export const routes: Routes = [
     {path: 'ofertas', component:OfertaComponent,},
-    {path: 'tiendas', component:TiendaComponent,},
+    {path: 'tiendas', component:TiendaComponent,
+        children:[
+            {path:':nuevo', component:CreaeditaTiendaComponent}
+        ]
+    },
 ];
 
